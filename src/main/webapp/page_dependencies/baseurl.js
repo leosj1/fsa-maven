156389ff9a9b656e64d5766d25e16250f0f3935f
leosj1 <johnsonseun15@gmail.com>
2021-01-04 09:42:36 -0800

    Baseurl change

--- src/main/webapp/page_dependencies/baseurl.js
+++ src/main/webapp/page_dependencies/baseurl.js
@@ -1,7 +1,8 @@
 
 //var app_url = "http://localhost:8015/FSA/";
-var app_url = "http://144.167.35.55/";
+//var app_url = "http://144.167.35.55/";
 //var app_url = "http://localhost:8015/ROOT/";
+var app_url = "http://cosmos-fsa.host.ualr.edu/";
 
   
 var baseurl =  app_url;
